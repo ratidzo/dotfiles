f3f711e6f7fcdf5151ccd133f086f0b8431c74f0
Ratidzo Goredema <ratygore@gmail.com>
2024-04-06 12:55:31 +0200

    neovim: set keymap '<leader>pv' to open project files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--Custom keymaps
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   -- Disable neotree. Using the default netrw instead.
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
 }
