782090860df39ab6969f4ef11f15be5c8cf714fa
Ratidzo Goredema <ratygore@gmail.com>
2024-04-07 10:38:58 +0200

    neovim/keymaps: set keymaps for moving editor windows

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,3 +25,9 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
