e343721301229185206aec4b23fd5cc833e9d29b
Ratidzo Goredema <ratygore@gmail.com>
2024-04-07 09:58:03 +0200

    kitty: Add kitty.conf and kitty directory to repo
    Created symlink to kitty directory in my .config
    Added some uncommitted changes in my neovim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
---Custom keymaps
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/ decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
