a9421fc66908434069836dce84c63355f7e34c79
Ratidzo Goredema <ratygore@gmail.com>
2024-04-07 10:31:05 +0200

    neovim: set keymaps for creating new editor tabs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,11 @@ keymap.set("n", "dw", "vb_d")
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", "tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
