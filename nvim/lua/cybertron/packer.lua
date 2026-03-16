9485202af5b607c29427d3ce858da829be862501
Ratidzo <ratygore@gmail.com>
2026-03-16 20:12:27 +0200

    nvim: setup telescope

--- nvim/lua/cybertron/packer.lua
+++ nvim/lua/cybertron/packer.lua
@@ -6,4 +6,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  use {
+      'nvim-telescope/telescope.nvim', tag = "*",
+      requires = {{ 'nvim-lua/plenary.nvim'}}
+  }
 end)
