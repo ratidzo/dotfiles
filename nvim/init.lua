df751ff972458077617b80a21659f9199cfe8bd8
Ratidzo <ratygore@gmail.com>
2026-03-17 13:43:37 +0200

    dev: setup keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,73 +1,10 @@
+vim.g.mapleader = " "
 
--- Set <space> as the leader key
--- See `:help mapleader`
--- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
+require("config.lazy")
+require("config.keymaps")
+require("config.options")
 
--- [[ Setting options ]] See `:h vim.o`
--- NOTE: You can change these options as you wish!
--- For more options, you can see `:help option-list`
--- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
--- (Note the single quotes)
 
--- Print the line number in front of each line
-vim.o.number = true
 
--- Use relative line numbers, so that it is easier to jump with j, k. This will affect the 'number'
--- option above, see `:h number_relativenumber`
-vim.o.relativenumber = true
-
--- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
-})
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Highlight the line where the cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- Show <tab> and trailing spaces
-vim.o.list = true
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
-vim.o.confirm = true
-
--- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
-
--- Use <Esc> to exit terminal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
-
--- [[ Basic Autocommands ]].
--- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
-
--- Highlight when yanking (copying) text.
--- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
-
--- [[ Create user commands ]]
--- See `:h nvim_create_user_command()` and `:h user-commands`
-
--- Create a command `:GitBlameLine` that print the git blame for the current line
-vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
-end, { desc = 'Print the git blame for the current line' })
 
 
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Set leader, h, j, k, l to navigate windows
+vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = 'Focus left' })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { desc = 'Focus down' })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { desc = 'Focus up' })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { desc = 'Focus down' })
+
+-- Split windows vertically and horizontally
+vim.keymap.set('n', '<leader>s', ':split<CR>', { desc = 'Horizontal split' })
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = 'Vertical split' })
+
+
+
+-- File explorer
+vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Launch file explorer' })
