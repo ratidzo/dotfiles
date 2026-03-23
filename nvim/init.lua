3967b5fab60fca501b83008ce970a45c6067a0da
Ratidzo <ratygore@gmail.com>
2026-03-24 00:51:48 +0200

    nvim: disable auto insertion of comment leaders

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,16 @@ require("config.options")
 
 require('nvim-treesitter').install {'c', 'rust', 'javascript', 'typescript', 'jsx', 'tsx', 'python' }
 
+
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
+
+-- disable auto insert of comment leaders
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
 
