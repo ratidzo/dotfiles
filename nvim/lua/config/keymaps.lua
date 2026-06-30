04bbbab1414056628e74c5a258078838a8eb9ae3
raty <ratygore@gmail.com>
2026-06-30 21:14:24 +0200

    nvim: set keymap for launching netrw explorer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+-- launch explorer (I'm using the default netrw)
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'launch netrw in current working directory' })
