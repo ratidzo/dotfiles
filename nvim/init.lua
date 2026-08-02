88597955775d9e8774b5feae72055a4c6c2c58f9
raty <ratygore@gmail.com>
2026-08-02 14:45:35 +0200

    nvim: turn off visible tab and space characters

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,7 @@ vim.o.smartcase = true
 
 vim.o.cursorline = true -- Highlight the line where the cursor is on.
 vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
-vim.o.list = true -- Show <tab> and trailing spaces.
+vim.o.list = false -- Do not show <tab> and trailing spaces.
 
 -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  opts = {
+  	ensure_installed = {
+		"typescript",
+		"c",
+		"python",
+		"lua"
+
+	}
+  }
+}
